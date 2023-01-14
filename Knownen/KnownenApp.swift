b2279ee1a38b96f0b6ad28597a2b1d13b05c85d5
Romi <romi.peli@pm.me>
2023-01-14 12:53:43 +0100

    Initial Commit

---
+++ Knownen/KnownenApp.swift
@@ -0,0 +1,17 @@
+//
+//  KnownenApp.swift
+//  Knownen
+//
+//  Created by Romi on 14/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct KnownenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
